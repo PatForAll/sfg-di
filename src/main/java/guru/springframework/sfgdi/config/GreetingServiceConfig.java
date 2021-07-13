22d5370ecf8317835dacd8f937686cd3aee55f0f
PatForAll <siekmeier.p@gmail.com>
2021-07-13 21:43:03 +0200

    remove service annotation from SetterGreetingService and use Configuration Class instead

---
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,20 @@
+package guru.springframework.sfgdi.config;
+
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import guru.springframework.sfgdi.services.SetterGreetingService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    ConstructorGreetingService constructorGreetingService() {
+        return new ConstructorGreetingService();
+    }
+
+    @Bean
+    SetterGreetingService setterGreetingService() {
+        return new SetterGreetingService();
+    }
+}

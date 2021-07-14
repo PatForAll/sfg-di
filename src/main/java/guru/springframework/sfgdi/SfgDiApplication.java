2ab648009e9b030ba856f548aa33b13a96984e23
PatForAll <siekmeier.p@gmail.com>
2021-07-14 22:00:44 +0200

    use Singleton & Prototype Bean Scope to show differences

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgdi;
 
 import guru.springframework.sfgdi.controllers.*;
+import guru.springframework.sfgdi.services.PrototypeBean;
+import guru.springframework.sfgdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -32,6 +34,16 @@ public class SfgDiApplication {
 		System.out.println("----------- Constructor");
 		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
 		System.out.println(constructorInjectedController.getGreeting());
+
+		SingletonBean singletonBean1 = (SingletonBean) ctx.getBean("singletonBean");
+		System.out.println(singletonBean1.getMyScope());
+		SingletonBean singletonBean2 = (SingletonBean) ctx.getBean("singletonBean");
+		System.out.println(singletonBean2.getMyScope());
+
+		PrototypeBean prototypeBean1 = (PrototypeBean) ctx.getBean("prototypeBean");
+		System.out.println(prototypeBean1.getMyScope());
+		PrototypeBean prototypeBean2 = (PrototypeBean) ctx.getBean("prototypeBean");
+		System.out.println(prototypeBean2.getMyScope());
 	}
 
 }
